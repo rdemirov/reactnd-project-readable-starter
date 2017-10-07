0bfd4d8fa1a762b1c0741c33fe28affb39b4c8b0
Radoslav Demirov <rdemirov@gmail.com>
2017-10-07 11:08:31 +0300

    fix:Remove missing module import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
