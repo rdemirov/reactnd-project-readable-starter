540159459d94aa66197ea2fc6e5890d8c3a0aa6f
Radoslav Demirov <rdemirov@gmail.com>
2017-10-21 18:25:44 +0300

    feat : Fetch categories and posts in the app component,to be able to generate the dynamic routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
 import Readable from './Readable';
 import NotFound from './NotFound';
+import {fetchPosts, fetchCategories} from '../actions';
 
 class App extends Component {
-				render() {
-								return (
-												<Switch>
-																<Route exact path='/' component={Readable}/>
-																<Route path='*' component={NotFound}/>
-												</Switch>
-								);
-				}
+    componentDidMount() {
+        const {fetchPosts,fetchCategories} = this.props;
+        fetchPosts();
+        fetchCategories();
+
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Readable}/>
+                <Route path="*" component={NotFound}/>
+            </Switch>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    posts: state.posts.postsArray,
+    showModal: state.posts.showModal,
+    categories: state.categories
+});
+
+const mapDispatchToProps = {
+    fetchPosts,
+    fetchCategories
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
